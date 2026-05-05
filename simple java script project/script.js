7adbd2ec580763a34492e5579d7ea27dc764b249
Rashed Ali <57hridoyrashed@gmail.com>
2026-05-05 19:36:39 +0600

    Add files via upload

---
+++ simple java script project/script.js
@@ -0,0 +1,24 @@
+alert("Hello");
+
+let userOneValue = prompt("User Input Data One");
+let userTwoValue = prompt("User Input Data Two");
+
+userOneValue = parseInt(userOneValue);
+userTwoValue = parseInt(userTwoValue);
+
+const Result = userOneValue + userTwoValue;
+const ResultTwo = userOneValue - userTwoValue;
+const ResultThree = userOneValue * userTwoValue;
+const ResultFour = userOneValue / userTwoValue;
+const ResultFive = userOneValue % userTwoValue;
+
+
+
+document.write("Input One : " + userOneValue + "<br>");
+document.write("Input Two : "+ userTwoValue + "<br>" + "<br>");
+
+document.write("output ADD : " + Result +"<br>" );
+document.write("output SUB : " + ResultTwo +"<br>" );
+document.write("output MUL : " + ResultThree +"<br>" );
+document.write("output DIV : " + ResultFour +"<br>" );
+document.write("output REM : " + ResultFive +"<br>" );
